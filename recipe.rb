f8d000dd0dbd911e76e32a5ae6a5e44d6ba747b6
Seiei Higa <hanachin@gmail.com>
2014-12-29 18:26:00 +0900

    install td-agent

--- recipe.rb
+++ recipe.rb
@@ -9,3 +9,8 @@ include_recipe 'oracle_jdk'
 include_recipe 'minecraft'
 include_recipe 'minecraft::overviewer'
 include_recipe 'rtn_rbenv::system'
+
+execute 'install td-agent' do
+  command 'curl -L http://toolbelt.treasuredata.com/sh/install-debian-squeeze-td-agent2.sh | sh'
+  not_if 'cat /etc/apt/sources.list | grep http://packages.treasuredata.com/2/debian/squeeze/'
+end
