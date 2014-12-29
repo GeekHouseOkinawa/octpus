38f5ee415810218291935eb10601691f0696d1d0
Seiei Higa <hanachin@gmail.com>
2014-12-29 22:03:00 +0900

    install curl for td-agent

--- recipe.rb
+++ recipe.rb
@@ -10,6 +10,7 @@ include_recipe 'minecraft'
 include_recipe 'minecraft::overviewer'
 include_recipe 'rtn_rbenv::system'
 
+package 'curl'
 execute 'install td-agent' do
   command 'curl -L http://toolbelt.treasuredata.com/sh/install-debian-squeeze-td-agent2.sh | sh'
   not_if 'cat /etc/apt/sources.list | grep http://packages.treasuredata.com/2/debian/squeeze/'
