e9670601f2eadac0807ea9e8af1972db07d07045
Seiei Higa <hanachin@gmail.com>
2014-12-29 21:23:12 +0900

    run overviewer by cron

--- recipe.rb
+++ recipe.rb
@@ -14,3 +14,18 @@ execute 'install td-agent' do
   command 'curl -L http://toolbelt.treasuredata.com/sh/install-debian-squeeze-td-agent2.sh | sh'
   not_if 'cat /etc/apt/sources.list | grep http://packages.treasuredata.com/2/debian/squeeze/'
 end
+
+template '/opt/minecraft/crontab' do
+  source 'crontab.erb'
+  owner node[:minecraft][:user]
+  group node[:minecraft][:user]
+end
+
+execute 'register minecraft overviewer' do
+  command 'crontab /opt/minecraft/crontab'
+  user    node[:minecraft][:user]
+end
+
+link '/usr/share/nginx/www/mcmap' do
+  to '/opt/minecraft/mcmap'
+end
